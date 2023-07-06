745472f5148f5f385190acb129527273b7458bba
Eric Ferreira <eric_fpereira@hotmail.com>
2023-07-06 12:34:35 -0300

    Pytest

---
+++ src/criador_nomes.py
@@ -0,0 +1,10 @@
+import random
+
+class RandomNames:
+    def generate_name(self):
+        names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve']
+        return random.choice(names)
+
+    def generate_surname(self):
+        surnames = ['Smith', 'Johnson', 'Brown', 'Taylor', 'Miller']
+        return random.choice(surnames)
--- test/test_pomodoro.py
+++
@@ -1,1 +0,0 @@
-from src.pomodoro_timer.pomodoro import *
