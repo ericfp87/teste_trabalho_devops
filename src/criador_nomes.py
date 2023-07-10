48ad80f59199b8172c0000f4c236271b967d96a7
Eric Ferreira <eric_fpereira@hotmail.com>
2023-07-09 21:46:36 -0300

    alteração de nomes

--- src/criador_nomes.py
+++ src/criador_nomes.py
@@ -3,9 +3,9 @@ import random
 
 class RandomNames:
     def generate_name(self):
-        names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Daniel']
+        names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Daniel', 'Eric']
         return random.choice(names)
 
     def generate_surname(self):
-        surnames = ['Smith', 'Johnson', 'Brown', 'Taylor', 'Miller', 'Bryan']
+        surnames = ['Smith', 'Johnson', 'Brown', 'Taylor', 'Miller', 'Bryan', 'Ferreira']
         return random.choice(surnames)
--- test/test_criador_nomes.py
+++ test/test_criador_nomes.py
@@ -4,11 +4,11 @@ from src.criador_nomes import RandomNames
 def test_generate_name():
     random_names = RandomNames()
     name = random_names.generate_name()
-    assert name in ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Daniel']
+    assert name in ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Daniel','Eric']
 
 
 def test_generate_surname():
     random_names = RandomNames()
     surname = random_names.generate_surname()
     assert surname in ['Smith', 'Johnson', 'Brown',
-                       'Taylor', 'Miller', 'Bryan']
+                       'Taylor', 'Miller', 'Bryan', 'Ferreira']
